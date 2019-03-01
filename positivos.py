70b2e06c321ff3f12916c52006ea0e6a3efec7ca
Maicol Bernal <maicol.bernal.2019@upb.edu.co>
2019-03-01 14:13:02 -0500

    Creacion Algoritmo

---
+++ positivos.py
@@ -0,0 +1,9 @@
+n=int(input("Digite El Primer Numero"))
+if n==0:
+    print("el numero es 0")
+else:
+    if n%2==0:
+        print("su numero es par")
+    else:
+        print("su numero no es par")
+        
